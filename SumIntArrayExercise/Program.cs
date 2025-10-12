f86294beec052ad14a3a4b6d2dd3c2267bfcfdac
giftos1 <giftsydney676@gmail.com>
2025-10-12 21:06:01 +0800

    Add numbers in Array

---
+++ SumIntArrayExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+namespace SumIntArrayExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            if (numbers.Length == 0)
+            {
+                Console.WriteLine("There are no numbers in the array");
+                return;
+            }
+            int sum = SumArray(numbers);
+            Console.WriteLine($"The sum of integers in array is {sum}");
+        }
+
+        static int SumArray(int[] nums)
+        {
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
